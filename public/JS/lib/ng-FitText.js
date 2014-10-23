7ea01b2e9986134ac4c27f69c64bcbde05abccef
zodiacsoulmate <zodiacsoulmate@gmail.com>
2014-10-22 23:06:49 -0400

    add debug page update library

--- public/JS/lib/ng-FitText.js
+++ public/JS/lib/ng-FitText.js
@@ -1,4 +1,4 @@
-/* ng-FitText.js v2.4.0
+/* ng-FitText.js v3.1.0
  * https://github.com/patrickmarabeas/ng-FitText.js
  *
  * Original jQuery project: https://github.com/davatron5000/FitText.js
@@ -8,65 +8,67 @@
  * Released under the MIT license
  * http://opensource.org/licenses/mit-license.php
  *
- * Date: 28/04/2014
+ * Date: 18/10/2014
  */
 
-'use strict';
+(function(window, document, angular, undefined) {
 
-angular.module( 'ngFitText', [] )
-  .value( 'config', {
-    'debounce': false,
-    'delay': 250,
-    'min': undefined,
-    'max': undefined
-  })
+  'use strict';
 
-  .directive( 'fittext', [ 'config', 'fitTextConfig', function( config, fitTextConfig ) {
-    return {
-      restrict: 'A',
-      scope: true,
-      transclude: true,
-      replace: true,
-      template: function( element, attrs ) {
-        var tag = element[0].nodeName;
-        return "<" + tag + " data-ng-transclude data-ng-style='{fontSize:fontSize}'></" + tag + ">";
-      },
-      link: function( scope, element, attrs ) {
-        angular.extend( config, fitTextConfig.config );
+  angular.module('ngFitText', [])
+    .value( 'config', {
+      'debounce': false,
+      'delay': 250,
+      'min': undefined,
+      'max': undefined
+    })
 
-        scope.compressor = attrs.fittext || 1;
-        scope.minFontSize = attrs.fittextMin || config.min || Number.NEGATIVE_INFINITY;
-        scope.maxFontSize = attrs.fittextMax || config.max || Number.POSITIVE_INFINITY;
-        scope.elementWidth = element[0].offsetWidth;
+    .directive('fittext', ['$timeout', 'config', 'fitTextConfig', function($timeout, config, fitTextConfig) {
+      return {
+        restrict: 'A',
+        scope: true,
+        link: function(scope, element, attrs) {
+          angular.extend(config, fitTextConfig.config);
 
-        ( scope.resizer = function() {
-          scope.elementWidth = element[0].offsetWidth;
-          scope.fontSize = Math.max(
-            Math.min(
-              scope.elementWidth / ( scope.compressor * 10 ),
-              parseFloat( scope.maxFontSize )
-            ),
-            parseFloat( scope.minFontSize )
-          ) + 'px';
+          element[0].style.display = 'inline-block';
+          element[0].style.whiteSpace = 'nowrap';
+          element[0].style.lineHeight = '1';
 
-        })();
+          var parent = element.parent();
+          var compressor = attrs.fittext || 1;
+          var minFontSize = attrs.fittextMin || config.min || Number.NEGATIVE_INFINITY;
+          var maxFontSize = attrs.fittextMax || config.max || Number.POSITIVE_INFINITY;
 
-        config.debounce == true
-          ? angular.element( window ).bind( 'resize', _debounce( function(){ scope.$apply( scope.resizer )}, config.delay ))
-          : angular.element( window ).bind( 'resize', function(){ scope.$apply( scope.resizer )});
+          var resizer = function() {
+            $timeout( function() {
+              var ratio = element[0].offsetHeight / element[0].offsetWidth;
+              element[0].style.fontSize = Math.max(
+                Math.min(parent[0].offsetWidth * ratio * compressor,
+                  parseFloat(maxFontSize)
+                ),
+                parseFloat(minFontSize)
+              ) + 'px';
+            },50);
+          }; resizer();
 
-        function _debounce(a,b,c){var d;return function(){var e=this,f=arguments;clearTimeout(d),d=setTimeout(function(){d=null,c||a.apply(e,f)},b),c&&!d&&a.apply(e,f)}}
+          scope.$watch(attrs.ngModel, function() { resizer() });
+
+          config.debounce
+            ? angular.element(window).bind('resize', config.debounce(function(){ scope.$apply(resizer)}, config.delay))
+            : angular.element(window).bind('resize', function(){ scope.$apply(resizer)});
+        }
       }
-    }
-  }])
+    }])
 
-  .provider( 'fitTextConfig', function() {
-    var self = this;
-    this.config = {};
-    this.$get = function() {
-      var extend = {};
-      extend.config = self.config;
-      return extend;
-    };
-    return this;
-  });
+    .provider('fitTextConfig', function() {
+      var self = this;
+      this.config = {};
+      this.$get = function() {
+        var extend = {};
+        extend.config = self.config;
+        return extend;
+      };
+      return this;
+    });
+
+})(window, document, angular);
